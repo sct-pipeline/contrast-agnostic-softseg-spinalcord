52dc099331a2cd77e46d5dff5ee3d649e73204b0
sandrinebedard <sandrine.bedard@polymtl.ca>
2022-07-06 14:18:13 -0400

    add json files of seg

--- merged_processing.sh
+++ merged_processing.sh
@@ -400,8 +400,8 @@ for file_path in "${inc_contrasts[@]}";do
   rsync -avzh $PATH_DATA_PROCESSED/${SUBJECT}/${fileseg}_crop.nii.gz $PATH_DATA_PROCESSED_CLEAN/derivatives/labels/${SUBJECT}/${fileseg}.nii.gz
   rsync -avzh $PATH_DATA_PROCESSED/${SUBJECT}/${filesoftseg}_crop.nii.gz $PATH_DATA_PROCESSED_CLEAN/derivatives/labels_softseg/${SUBJECT}/${filesoftseg}.nii.gz
   # Move json files of derivatives
-  rsync -avzh "${PATH_DATA}/derivatives/labels_softseg/${SUBJECT}/${filesoftseg}.json"
-  rsync -avzh "${PATH_DATA}/derivatives/labels/${SUBJECT}/${fileseg}.json"
+  rsync -avzh "${PATH_DATA}/derivatives/labels_softseg/${SUBJECT}/${filesoftseg}.json" $PATH_DATA_PROCESSED_CLEAN/derivatives/labels_softseg/${SUBJECT}/${filesoftseg}.json
+  rsync -avzh "${PATH_DATA}/derivatives/labels/${SUBJECT}/${fileseg}.json" $PATH_DATA_PROCESSED_CLEAN/derivatives/labels/${SUBJECT}/${fileseg}.nii.gz
 
 done
 
