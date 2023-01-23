86c6cf85e4775ed85a3aee362daa485b17f3fe54
Adrian El Baz <adelba@romane.neuro.polymtl.ca>
2023-01-23 10:12:18 -0500

    Inference bash script to segment test participants MRIs

--- inference.sh
+++ inference.sh
@@ -1,6 +1,6 @@
 seeds=(15) # seeds=(42 15 34 98 62)
 config_types=("hard_hard" "hard_soft" "meanGT_soft" "meanGT_hard")
-contrasts=("T1w" "T2star" "T2w" "rec-average_dwi" "all")
+contrasts=("flip-1_mt-on_MTS" "flip-2_mt-off_MTS" "all")
 
 for seed in ${seeds[@]}
 do
@@ -9,9 +9,9 @@ do
     for contrast in ${contrasts[@]}
     do
       sleep 5
-      output_dir=../duke/temp/adrian/contrast-agnostic-seg-models/Group8_01-12-2022/"$config"_"$contrast"_seed="$seed"/
+      output_dir=../duke/temp/adrian/contrast-agnostic-seg-models/Group9_20-12-2022/"$config"_"$contrast"_seed="$seed"/
       echo $output_dir
-      CUDA_VISIBLE=0 ivadomed --test -c ../duke/temp/adrian/contrast-agnostic-seg-models/Group8_01-12-2022/"$config"_"$contrast"_seed="$seed"/config_file.json --path-output $output_dir
+      CUDA_VISIBLE=0 ivadomed --test -c ../duke/temp/adrian/contrast-agnostic-seg-models/Group9_20-12-2022/"$config"_"$contrast"_seed="$seed"/config_file.json --path-output $output_dir
     done
   done
 done
