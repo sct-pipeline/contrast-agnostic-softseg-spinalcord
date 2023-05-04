4b114226163319ee4519b975d66865d5b42e800f
sandrinebedard <sandrine.bedard@polymtl.ca>
2023-05-04 17:50:05 -0400

    change prefix

--- run_qc_prediction.sh
+++ run_qc_prediction.sh
@@ -83,7 +83,7 @@ for file_pred in ${PATH_PRED_SEG}/*; do
 
         # rsync prediction mask
         rsync -avzh $file_pred ${type}/$file_seg_basename
-        prefix="spineGNoCropNoThr_"  #TODO  
+        prefix="spineGNoCropThr_"  #TODO  
         file_image=${file_seg_basename#"$prefix"}
         echo $file_image
         file_image="${file_image::-11}"  # Remove X.nii.gz since teh number X varies
