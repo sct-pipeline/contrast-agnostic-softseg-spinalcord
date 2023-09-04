c59edac28e65e3f358411c5167a844a6118305b7
Naga Karthik <emvnagakarthik@gmail.com>
2023-09-04 14:49:44 -0400

    update train transforms like nnunet's

--- monai/transforms.py
+++ monai/transforms.py
@@ -1,15 +1,17 @@
 
 import numpy as np
 from monai.transforms import (SpatialPadd, Compose, CropForegroundd, LoadImaged, RandFlipd, 
-            RandCropByPosNegLabeld, Spacingd, RandRotated, NormalizeIntensityd, RandAffined,
+            RandCropByPosNegLabeld, Spacingd, RandScaleIntensityd, NormalizeIntensityd, RandAffined,
             RandWeightedCropd, RandAdjustContrastd, EnsureChannelFirstd, RandGaussianNoised, 
-            RandGaussianSmoothd, Orientationd, Rand3DElasticd, RandBiasFieldd, RandSimulateLowResolutiond)
+            RandGaussianSmoothd, Orientationd, Rand3DElasticd, RandBiasFieldd, RandSimulateLowResolutiond,
+            ResizeWithPadOrCropd)
 # import torchio as tio
 
 # median image size in voxels - taken from nnUNet
-# median_size = (123, 255, 214)     # so pad with this size
+# median_size = (123, 255, 214)  as per 0.9 iso resampling and patch_size = (80, 192, 160)
+# note the the order of the axes is different in nnunet and monai (dims 0 and 2 are swapped)
 # median_size after 1mm isotropic resampling
-# median_size = [ 192. 228. 106.]
+# median_size = [ 192. 228. 106.]   
 
 # Order in which nnunet does preprocessing:
 # 1. Crop to non-zero
@@ -26,24 +28,28 @@ def train_transforms(crop_size, num_samples_pv, lbl_key="label"):
         NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),
         Spacingd(keys=["image", "label"], pixdim=(1.0, 1.0, 1.0), mode=("bilinear", "bilinear"),),
         # data-augmentation
-        SpatialPadd(keys=["image", lbl_key], spatial_size=(192, 228, 106), method="symmetric"),
+        # SpatialPadd(keys=["image", lbl_key], spatial_size=(192, 228, 106), method="symmetric"),
+        SpatialPadd(keys=["image", lbl_key], spatial_size=crop_size, method="symmetric"),   # pad with the same size as crop_size
         # NOTE: used with neg together to calculate the ratio pos / (pos + neg) for the probability to pick a 
         # foreground voxel as a center rather than a background voxel.
         RandCropByPosNegLabeld(keys=["image", "label"], label_key="label",
                             spatial_size=crop_size, pos=2, neg=1, num_samples=num_samples_pv, 
                             # if num_samples=4, then 4 samples/image are randomly generated
                             image_key="image", image_threshold=0.),
-        # transforms used by ivadomed and nnunet
-        RandAffined(keys=["image", lbl_key], mode=("bilinear", "nearest"), prob=1.0,
+        # ResizeWithPadOrCropd(keys=["image", lbl_key], spatial_size=crop_size,),
+        
+        # re-ordering transforms as used by nnunet
+        RandAffined(keys=["image", lbl_key], mode=("bilinear", "bilinear"), prob=0.25,
                     rotate_range=(-20.0, 20.0), scale_range=(0.8, 1.2), translate_range=(-0.1, 0.1)),
-        Rand3DElasticd(keys=["image", lbl_key], sigma_range=(3.5, 5.5), magnitude_range=(25, 35), prob=0.5),   
+        # Rand3DElasticd(keys=["image", lbl_key], sigma_range=(3.5, 5.5), magnitude_range=(25, 35), prob=0.5),
+        RandGaussianNoised(keys=["image"], mean=0.0, std=0.1, prob=0.1),
+        RandGaussianSmoothd(keys=["image"], sigma_x=(0.5, 1.0), sigma_y=(0.5, 1.0), sigma_z=(0.5, 1.0), prob=0.25),
+        RandScaleIntensityd(keys=["image"], factors=(-0.25, 1), prob=0.15),  # this is nnUNet's BrightnessMultiplicativeTransform
         RandSimulateLowResolutiond(keys=["image"], zoom_range=(0.5, 1.0), prob=0.25),
-        RandAdjustContrastd(keys=["image"], gamma=(0.5, 1.5), prob=0.5),    # this is monai's RandomGamma
+        RandAdjustContrastd(keys=["image"], gamma=(0.5, 1.5), prob=0.3),    # this is monai's RandomGamma
         RandBiasFieldd(keys=["image"], coeff_range=(0.0, 0.5), degree=3, prob=0.3),
-        RandGaussianSmoothd(keys=["image"], sigma_x=(0.0, 2.0), sigma_y=(0.0, 2.0), sigma_z=(0.0, 2.0), prob=0.3),
-        # RandGaussianNoised(keys=["image"], mean=0.0, std=0.1, prob=0.1),
         # TODO: Try Spacingd with low resolution here with prob=0.5
-        # RandFlipd(keys=["image", lbl_key], spatial_axis=None, prob=0.4,),
+        RandFlipd(keys=["image", lbl_key], prob=0.5,),
         # RandRotated(keys=["image", lbl_key], mode=("bilinear", "nearest"), prob=0.2,
         #             range_x=(-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi),  # NOTE: -pi/6 to pi/6
         #             range_y=(-30. / 360 * 2. * np.pi, 30. / 360 * 2. * np.pi), 
