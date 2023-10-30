d79b133ca8f3b6f7d1a6574e58132ec56c37ebe1
sandrinebedard <sandrine.bedard@polymtl.ca>
2023-10-30 09:47:29 -0700

    change contrast

--- qc_other_datasets/run_qc_prediction_whole_spine.sh
+++ qc_other_datasets/run_qc_prediction_whole_spine.sh
@@ -75,7 +75,7 @@ file="${file_sub}_T1w"
 python $PATH_SCRIPT/monai/run_inference_single_image.py --path-img ${file}.nii.gz --path-out . --chkp-path ~/duke/temp/muena/contrast-agnostic/final_monai_model/nnunet_nf=32_DS=1_opt=adam_lr=0.001_AdapW_CCrop_bs=2_64x192x320_20230918-2253
 sct_maths -i ${file}_pred.nii.gz -bin 0.5 -o ${file}_pred_bin.nii.gz
 sct_qc -i ${file}.nii.gz -s ${file}_pred_bin.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
-sct_deepseg_sc -i ${file}.nii.gz -c t2 -qc ${PATH_QC} -qc-subject ${SUBJECT}
+sct_deepseg_sc -i ${file}.nii.gz -c t1 -qc ${PATH_QC} -qc-subject ${SUBJECT}
 
 
 
