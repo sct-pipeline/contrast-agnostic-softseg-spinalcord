89398035cb67fad2bfcea4c3335dd0f55eccb7a8
valosekj <jan.valosek@upol.cz>
2024-01-08 15:48:44 -0500

    Plot STD across participants for each method

--- csa_across_resolutions/generate_figure_csa_across_resolutions.py
+++ csa_across_resolutions/generate_figure_csa_across_resolutions.py
@@ -5,12 +5,15 @@ Usage:
     python generate_figure_csa_across_resolutions.py -i /path/to/data.csv
 """
 
+import os
 import argparse
 import pandas as pd
 import re
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+# Setting the hue order as specified
+HUE_ORDER = ["propseg", "deepseg_2d", "nnunet_3d_fullres", "monai"]
 
 # Function to extract method and resolution from the filename
 def extract_method_resolution(filename):
@@ -27,26 +30,39 @@ def extract_method_resolution(filename):
         return 'Unknown', 'Unknown'
 
 
-def generate_figure(data, file_path):
+def fetch_participant_id(filename_path):
+    """
+    Get participant_id from the input BIDS-compatible filename or file path
+    :return: participant_id: subject ID (e.g., sub-001)
+    """
+
+    _, filename = os.path.split(filename_path)              # Get just the filename (i.e., remove the path)
+    participant = re.search('sub-(.*?)[_/]', filename_path)     # [_/] slash or underscore
+    participant_id = participant.group(0)[:-1] if participant else ""    # [:-1] removes the last underscore or slash
+    # REGEX explanation
+    # \d - digit
+    # \d? - no or one occurrence of digit
+    # *? - match the previous element as few times as possible (zero or more times)
+    # . - any character
+
+    return participant_id
+
+
+def generate_figure(data, contrast, file_path):
     """
     Generate violinplot across resolutions and methods
     :param data: Pandas DataFrame with the data
+    :param contrast: Contrast (e.g., T1w, T2w, T2star)
     :param file_path: Path to the CSV file (will be used to save the figure)
     """
-
-    # Fetch contrast (e.g. T1w, T2w, T2star) from the first filename using regex
-    contrast = re.search(r'.*_(T1w|T2w|T2star).*', data['Filename'][0]).group(1)
 
     # Correct labels for the x-axis based on the actual data
     resolution_labels = ['1mm', '125mm', '15mm', '175mm', '2mm']
 
-    # Setting the hue order as specified
-    hue_order = ["propseg", "deepseg_2d", "nnunet_3d_fullres", "monai"]
-
     # Creating the violin plot
     plt.figure(figsize=(12, 6))
     sns.violinplot(x='Resolution', y='MEAN(area)', hue='Method', data=data, order=resolution_labels,
-                   hue_order=hue_order)
+                   hue_order=HUE_ORDER)
     plt.xticks(rotation=45)
     plt.xlabel('Resolution')
     plt.ylabel('CSA [mm^2]')
@@ -66,6 +82,29 @@ def generate_figure(data, file_path):
 
     # Display the plot
     plt.show()
+
+
+def generate_figure_std(data, contrast, file_path):
+    """
+    Generate violinplot showing STD across participants for each method
+    """
+
+    # Compute mean and std across resolutions for each method
+    df = data.groupby(['Method', 'Participant'])['MEAN(area)'].agg(['mean', 'std']).reset_index()
+
+    plt.figure(figsize=(12, 6))
+    sns.violinplot(x='Method', y='std', data=df, order=HUE_ORDER)
+    plt.xticks(rotation=45)
+    plt.xlabel('Method')
+    plt.ylabel('STD [mm^2]')
+    plt.title(f'{contrast}: STD of C2-C3 CSA across resolutions for each method')
+    # Add horizontal dashed grid
+    plt.grid(axis='y', alpha=0.5, linestyle='dashed')
+
+    # Save the figure in 300 DPI as a PNG file
+    plt.tight_layout()
+    plt.savefig(file_path.replace('.csv', '_STD.png'), dpi=300)
+    print(f'Figure saved to {file_path.replace(".csv", "_STD.png")}')
 
 
 def compute_cov(data, file_path):
@@ -98,8 +137,17 @@ def main(file_path):
     # Apply the function to extract method and resolution
     data['Method'], data['Resolution'] = zip(*data['Filename'].apply(extract_method_resolution))
 
+    # Apply the function to extract participant ID
+    data['Participant'] = data['Filename'].apply(fetch_participant_id)
+
+    # Fetch contrast (e.g. T1w, T2w, T2star) from the first filename using regex
+    contrast = re.search(r'.*_(T1w|T2w|T2star).*', data['Filename'][0]).group(1)
+
     # Generate violinplot across resolutions and methods
-    generate_figure(data, file_path)
+    generate_figure(data, contrast, file_path)
+
+    # Generate violinplot showing STD across participants for each method
+    generate_figure_std(data, contrast, file_path)
 
     # Compute COV for CSA for each method across resolutions
     compute_cov(data, file_path)
