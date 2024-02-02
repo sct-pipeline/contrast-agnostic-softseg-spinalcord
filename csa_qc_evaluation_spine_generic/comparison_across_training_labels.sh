edae2ee976f440be64eb7f610eee4ab5974a0db9
Naga Karthik <emvnagakarthik@gmail.com>
2024-02-02 04:20:33 -0500

    add script to compare csa across training GT

---
+++ csa_qc_evaluation_spine_generic/comparison_across_training_labels.sh
@@ -0,0 +1,356 @@
+#!/bin/bash
+#
+# Compare the CSA of soft GT thresholded at different values on spine-generic test dataset.
+# 
+# Adapted from: https://github.com/ivadomed/model_seg_sci/blob/main/baselines/comparison_with_other_methods_sc.sh
+#
+# Usage:
+#     sct_run_batch -config config.json
+#
+# Example of config.json:
+# {
+#  "path_data"   : "<PATH_TO_DATASET>",
+#  "path_output" : "<PATH_TO_DATASET>_2023-08-18",
+#  "script"      : "<PATH_TO_REPO>/model_seg_sci/baselines/comparison_with_other_methods.sh",
+#  "jobs"        : 8,
+#  "script_args" : "<PATH_TO_REPO>/model_seg_sci/packaging/run_inference_single_subject.py <PATH_TO_MODEL>/sci-multisite-model <PATH_TO_CONTRAST-AGNOSTIC_REPO>/monai/run_inference_single_image.py <PATH_TO_CONTRAST-AGNOSTIC_MODEL>"
+# }
+#
+# The following global variables are retrieved from the caller sct_run_batch
+# but could be overwritten by uncommenting the lines below:
+# PATH_DATA_PROCESSED="~/data_processed"
+# PATH_RESULTS="~/results"
+# PATH_LOG="~/log"
+# PATH_QC="~/qc"
+#
+# Author: Jan Valosek and Naga Karthik
+#
+
+# Uncomment for full verbose
+set -x
+
+# Immediately exit if error
+set -e -o pipefail
+
+# Exit if user presses CTRL+C (Linux) or CMD+C (OSX)
+trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
+
+# Print retrieved variables from the sct_run_batch script to the log (to allow easier debug)
+echo "Retrieved variables from from the caller sct_run_batch:"
+echo "PATH_DATA: ${PATH_DATA}"
+echo "PATH_DATA_PROCESSED: ${PATH_DATA_PROCESSED}"
+echo "PATH_RESULTS: ${PATH_RESULTS}"
+echo "PATH_LOG: ${PATH_LOG}"
+echo "PATH_QC: ${PATH_QC}"
+
+SUBJECT=$1
+PATH_NNUNET_SCRIPT=$2   # path to the nnUNet contrast-agnostic run_inference_single_subject.py
+PATH_NNUNET_MODEL=$3    # path to the nnUNet contrast-agnostic model
+PATH_MONAI_SCRIPT=$4    # path to the MONAI contrast-agnostic run_inference_single_subject.py
+PATH_MONAI_MODEL_SOFT=$5     # path to the MONAI contrast-agnostic model trained on soft labels
+PATH_MONAI_MODEL_SOFTBIN=$6     # path to the MONAI contrast-agnostic model trained on soft_bin labels
+
+echo "SUBJECT: ${SUBJECT}"
+echo "PATH_NNUNET_SCRIPT: ${PATH_NNUNET_SCRIPT}"
+echo "PATH_NNUNET_MODEL: ${PATH_NNUNET_MODEL}"
+echo "PATH_MONAI_SCRIPT: ${PATH_MONAI_SCRIPT}"
+echo "PATH_MONAI_MODEL_SOFT: ${PATH_MONAI_MODEL_SOFT}"
+echo "PATH_MONAI_MODEL_SOFTBIN: ${PATH_MONAI_MODEL_SOFTBIN}"
+
+# ------------------------------------------------------------------------------
+# CONVENIENCE FUNCTIONS
+# ------------------------------------------------------------------------------
+
+# Check if manual label already exists. If it does, copy it locally.
+# NOTE: manual disc labels should go from C1-C2 to C7-T1.
+label_vertebrae(){
+  local file="$1"
+  local contrast="$2"
+  
+  # Update global variable with segmentation file name
+  FILESEG="${file}_seg-manual"
+  FILELABEL="${file}_discs"
+
+  # Label vertebral levels
+  sct_label_utils -i ${file}.nii.gz -disc ${FILELABEL}.nii.gz -o ${FILESEG}_labeled.nii.gz
+
+  # # Run QC
+  # sct_qc -i ${file}.nii.gz -s ${file_seg}_labeled.nii.gz -p sct_label_vertebrae -qc ${PATH_QC} -qc-subject ${SUBJECT}
+}
+
+
+# Copy GT spinal cord disc labels (located under derivatives/labels)
+copy_gt_disc_labels(){
+  local file="$1"
+  local type="$2"
+  # Construct file name to GT segmentation located under derivatives/labels
+  FILEDISCLABELS="${PATH_DATA}/derivatives/labels/${SUBJECT}/${type}/${file}_discs.nii.gz"
+  echo ""
+  echo "Looking for manual disc labels: $FILEDISCLABELS"
+  if [[ -e $FILEDISCLABELS ]]; then
+      echo "Found! Copying ..."
+      rsync -avzh $FILEDISCLABELS ${file}_discs.nii.gz
+  else
+      echo "File ${FILEDISCLABELS} does not exist" >> ${PATH_LOG}/missing_files.log
+      echo "ERROR: Manual Disc Labels ${FILEDISCLABELS} does not exist. Exiting."
+      exit 1
+  fi
+}
+
+# Copy GT segmentation (located under derivatives/labels)
+copy_gt_seg(){
+  local file="$1"
+  local type="$2"
+  # Construct file name to GT segmentation located under derivatives/labels
+  FILESEG="${PATH_DATA}/derivatives/labels/${SUBJECT}/${type}/${file}_seg-manual.nii.gz"
+  echo ""
+  echo "Looking for manual segmentation: $FILESEG"
+  if [[ -e $FILESEG ]]; then
+      echo "Found! Copying ..."
+      rsync -avzh $FILESEG ${file}_seg-manual.nii.gz
+  else
+      echo "File ${FILESEG}.nii.gz does not exist" >> ${PATH_LOG}/missing_files.log
+      echo "ERROR: Manual Segmentation ${FILESEG} does not exist. Exiting."
+      exit 1
+  fi
+}
+
+# Copy GT soft segmentation (located under derivatives/labels_softseg)
+copy_gt_softseg(){
+  local file="$1"
+  local type="$2"
+  # Construct file name to GT segmentation located under derivatives/labels
+  FILESEG="${PATH_DATA}/derivatives/labels_softseg/${SUBJECT}/${type}/${file}_softseg.nii.gz"
+  echo ""
+  echo "Looking for manual segmentation: $FILESEG"
+  if [[ -e $FILESEG ]]; then
+      echo "Found! Copying ..."
+      rsync -avzh $FILESEG ${file}_softseg.nii.gz
+  else
+      echo "File ${FILESEG} does not exist" >> ${PATH_LOG}/missing_files.log
+      echo "ERROR: Manual Segmentation ${FILESEG} does not exist. Exiting."
+      exit 1
+  fi
+}
+
+
+# TODO: Fix the contrast input for deepseg and propseg (i.e. dwi, mton, mtoff won't work)
+# Segment spinal cord using methods available in SCT (sct_deepseg_sc or sct_propseg), resample the prediction back to
+# native resolution and compute CSA in native space
+segment_sc() {
+  local file="$1"
+  local contrast="$2"
+  local method="$3"     # deepseg or propseg
+  local kernel="$4"     # 2d or 3d; only relevant for deepseg
+  local file_gt_vert_label="$5"
+  local native_res="$6"
+
+  # Segment spinal cord
+  if [[ $method == 'deepseg' ]];then
+      FILESEG="${file}_seg_${method}_${kernel}"
+
+      # Get the start time
+      start_time=$(date +%s)
+      # Run SC segmentation
+      sct_deepseg_sc -i ${file}.nii.gz -o ${FILESEG}.nii.gz -c ${contrast} -kernel ${kernel} -qc ${PATH_QC} -qc-subject ${SUBJECT}
+      # Get the end time
+      end_time=$(date +%s)
+      # Calculate the time difference
+      execution_time=$(python3 -c "print($end_time - $start_time)")
+      echo "${FILESEG},${execution_time}" >> ${PATH_RESULTS}/execution_time.csv
+  
+  elif [[ $method == 'propseg' ]]; then
+      FILESEG="${file}_seg_${method}"
+
+      # Get the start time
+      start_time=$(date +%s)
+      # Run SC segmentation
+      sct_propseg -i ${file}.nii.gz -o ${FILESEG}.nii.gz -c ${contrast} -qc ${PATH_QC} -qc-subject ${SUBJECT}
+      # Get the end time
+      end_time=$(date +%s)
+      # Calculate the time difference
+      execution_time=$(python3 -c "print($end_time - $start_time)")
+      echo "${FILESEG},${execution_time}" >> ${PATH_RESULTS}/execution_time.csv
+
+      # Remove centerline (we don't need it)
+      rm ${file}_centerline.nii.gz
+
+  fi
+
+  # Compute CSA from the the SC segmentation resampled back to native resolution using the GT vertebral labels
+  sct_process_segmentation -i ${FILESEG}.nii.gz -vert 2:4 -vertfile ${file_gt_vert_label}_labeled.nii.gz -o $PATH_RESULTS/csa_label_types_c24.csv -append 1
+
+}
+
+# Segment spinal cord using the contrast-agnostic nnUNet model
+segment_sc_nnUNet(){
+  local file="$1"
+  local kernel="$2"     # 2d or 3d
+  local file_gt_vert_label="$3"
+  local contrast="$4"   # used only for saving output file name
+
+  # FILESEG="${file}_seg_nnunet_${kernel}"
+  FILESEG="${file%%_*}_${contrast}_seg_nnunet"
+
+  # Get the start time
+  start_time=$(date +%s)
+  # Run SC segmentation
+  python ${PATH_NNUNET_SCRIPT} -i ${file}.nii.gz -o ${FILESEG}.nii.gz -path-model ${PATH_NNUNET_MODEL}/nnUNetTrainer__nnUNetPlans__${kernel} -pred-type sc -use-gpu
+  # Get the end time
+  end_time=$(date +%s)
+  # Calculate the time difference
+  execution_time=$(python3 -c "print($end_time - $start_time)")
+  echo "${FILESEG},${execution_time}" >> ${PATH_RESULTS}/execution_time.csv
+
+  # # Generate QC report
+  # sct_qc -i ${file}.nii.gz -s ${FILESEG}.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
+
+  # Compute CSA from the prediction resampled back to native resolution using the GT vertebral labels
+  sct_process_segmentation -i ${FILESEG}.nii.gz -vert 2:4 -vertfile ${file_gt_vert_label}_labeled.nii.gz -o $PATH_RESULTS/csa_label_types_c24.csv -append 1
+
+}
+
+# Segment spinal cord using the MONAI contrast-agnostic model
+segment_sc_MONAI(){
+  local file="$1"
+  local file_gt_vert_label="$2"
+  local label_type="$3"     # soft or soft_bin
+  local contrast="$4"   # used only for saving output file name
+
+	if [[ $label_type == 'soft' ]]; then
+		FILESEG="${file%%_*}_${contrast}_seg_monai_soft"
+		PATH_MONAI_MODEL=${PATH_MONAI_MODEL_SOFT}
+	
+	elif [[ $label_type == 'soft_bin' ]]; then
+		FILESEG="${file%%_*}_${contrast}_seg_monai_bin"
+		PATH_MONAI_MODEL=${PATH_MONAI_MODEL_SOFTBIN}
+	
+	fi
+
+  # Get the start time
+  start_time=$(date +%s)
+  # Run SC segmentation
+  python ${PATH_MONAI_SCRIPT} --path-img ${file}.nii.gz --path-out . --chkp-path ${PATH_MONAI_MODEL} --device gpu
+  # Rename MONAI output
+  mv ${file}_pred.nii.gz ${FILESEG}.nii.gz
+  # Get the end time
+  end_time=$(date +%s)
+  # Calculate the time difference
+  execution_time=$(python3 -c "print($end_time - $start_time)")
+  echo "${FILESEG},${execution_time}" >> ${PATH_RESULTS}/execution_time.csv
+
+  # Binarize MONAI output (which is soft by default); output is overwritten
+  sct_maths -i ${FILESEG}.nii.gz -bin 0.5 -o ${FILESEG}.nii.gz
+
+  # Generate QC report with soft prediction
+  sct_qc -i ${file}.nii.gz -s ${FILESEG}.nii.gz -p sct_deepseg_sc -qc ${PATH_QC} -qc-subject ${SUBJECT}
+
+  # Compute CSA from the soft prediction resampled back to native resolution using the GT vertebral labels
+  sct_process_segmentation -i ${FILESEG}.nii.gz -vert 2:4 -vertfile ${file_gt_vert_label}_labeled.nii.gz -o $PATH_RESULTS/csa_label_types_c24.csv -append 1
+}
+
+
+# ------------------------------------------------------------------------------
+# SCRIPT STARTS HERE
+# ------------------------------------------------------------------------------
+# get starting time:
+start=`date +%s`
+
+# Display useful info for the log, such as SCT version, RAM and CPU cores available
+sct_check_dependencies -short
+
+# Go to folder where data will be copied and processed
+cd $PATH_DATA_PROCESSED
+
+# Copy source images
+# Note: we use '/./' in order to include the sub-folder 'ses-0X'
+# We do a substitution '/' --> '_' in case there is a subfolder 'ses-0X/'
+rsync -Ravzh ${PATH_DATA}/./${SUBJECT}/anat/* .
+# copy DWI data
+rsync -Ravzh ${PATH_DATA}/./${SUBJECT}/dwi/* .
+
+# ------------------------------------------------------------------------------
+# contrast
+# ------------------------------------------------------------------------------
+contrasts="T1w T2w T2star flip-1_mt-on_MTS flip-2_mt-off_MTS rec-average_dwi"
+# contrasts="flip-2_mt-off_MTS rec-average_dwi"
+
+# Loop across contrasts
+for contrast in ${contrasts}; do
+
+  if [[ $contrast == "rec-average_dwi" ]]; then
+    type="dwi"
+  else
+    type="anat"
+  fi
+
+  # go to the folder where the data is
+  cd ${PATH_DATA_PROCESSED}/${SUBJECT}/${type}
+
+  # Get file name
+  file="${SUBJECT}_${contrast}"
+
+  # Check if file exists
+  if [[ ! -e ${file}.nii.gz ]]; then
+      echo "File ${file}.nii.gz does not exist" >> ${PATH_LOG}/missing_files.log
+      echo "ERROR: File ${file}.nii.gz does not exist. Exiting."
+      exit 1
+  fi
+
+  # Copy GT spinal cord segmentation
+  copy_gt_seg "${file}" "${type}"
+
+  # Copy soft GT spinal cord segmentation
+  copy_gt_softseg "${file}" "${type}"
+
+  # Copy GT disc labels segmentation
+  copy_gt_disc_labels "${file}" "${type}"
+
+  # Label vertebral levels in the native resolution
+  label_vertebrae ${file} 't2'
+
+  # rename contrasts 
+  if [[ $contrast == "flip-1_mt-on_MTS" ]]; then
+    contrast="MTon"
+  elif [[ $contrast == "flip-2_mt-off_MTS" ]]; then
+    contrast="MToff"
+  elif [[ $contrast == "rec-average_dwi" ]]; then
+    contrast="DWI"
+  fi
+
+  # 1. Compute (soft) CSA of the original soft GT
+  # renaming file so that it can be fetched from the CSA csa file later 
+  FILEINPUT="${file%%_*}_${contrast}_softseg"
+  cp ${file}_softseg.nii.gz ${FILEINPUT}.nii.gz
+  sct_process_segmentation -i ${FILEINPUT}.nii.gz -vert 2:4 -vertfile ${file}_seg-manual_labeled.nii.gz -o $PATH_RESULTS/csa_label_types_c24.csv -append 1
+
+  # Threshold the soft GT 
+  FILETHRESH="${file%%_*}_${contrast}_softseg_bin"
+  sct_maths -i ${file}_softseg.nii.gz -bin 0.5 -o ${FILETHRESH}.nii.gz
+
+  # 2. Compute CSA of the binarized soft GT 
+  sct_process_segmentation -i ${FILETHRESH}.nii.gz -vert 2:4 -vertfile ${file}_seg-manual_labeled.nii.gz -o $PATH_RESULTS/csa_label_types_c24.csv -append 1
+
+  # 3. Segment SC using different methods, binarize at 0.5 and compute CSA
+  segment_sc_MONAI ${file} "${file}_seg-manual" 'soft' ${contrast}
+	segment_sc_MONAI ${file} "${file}_seg-manual" 'soft_bin' ${contrast}
+  segment_sc_nnUNet ${file} '3d_fullres' "${file}_seg-manual" ${contrast}
+	# # TODO: run on deep/progseg after fixing the contrasts for those
+  # segment_sc ${file_res} 't2' 'deepseg' '2d' "${file}_seg-manual" ${native_res}
+  # segment_sc ${file_res} 't2' 'propseg' '' "${file}_seg-manual" ${native_res}
+
+done
+
+# ------------------------------------------------------------------------------
+# End
+# ------------------------------------------------------------------------------
+
+# Display results (to easily compare integrity across SCT versions)
+end=`date +%s`
+runtime=$((end-start))
+echo
+echo "~~~"
+echo "SCT version: `sct_version`"
+echo "Ran on:      `uname -nsr`"
+echo "Duration:    $(($runtime / 3600))hrs $((($runtime / 60) % 60))min $(($runtime % 60))sec"
+echo "~~~"
