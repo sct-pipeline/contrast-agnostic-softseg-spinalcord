fcd13ef7ffcb939ab4d70959c86fc6a384030928
Naga Karthik <emvnagakarthik@gmail.com>
2024-07-22 05:26:14 -0400

    add script for running ablations on contrasts

--- monai/run_ablations.sh
+++ monai/run_ablations.sh
@@ -1,34 +1,58 @@
 #!/bin/bash
 
-# # This script runs the ablation study on the different contrasts
-# # for the contrast-agnostic model with soft segmentation labels.
+# This script runs the ablation study on the different contrasts
+# for the contrast-agnostic model with soft segmentation labels.
 
 
 # ablations=("t2w_t1w_dwi_mtoff_t2star" "t2w_t1w_dwi_mtoff" "t2w_t1w_dwi" "t2w_t1w" "t2w")
-# cuda_device=1
+ablations=("t2w_t1w_dwi_t2star")
+cuda_device=3
 
-# for ablation in "${ablations[@]}"; do
+for ablation in "${ablations[@]}"; do
 
-#     echo ""
-#     echo "-------------------------------------------------"
-#     echo "Running ablation on contrast(s): ${ablation}"
-#     echo "-------------------------------------------------"
+    echo ""
+    echo "-------------------------------------------------"
+    echo "Running ablation on contrast(s): ${ablation}"
+    echo "-------------------------------------------------"
 
-#     # run the model
-#     CUDA_VISIBLE_DEVICES=${cuda_device} python monai/main.py \
-#         -r /home/GRAMES.POLYMTL.CA/u114716/contrast-agnostic/datalists/spine-generic-contrast-ablations/seed15 \
-#         -m nnunet -crop 64x192x320 \
-#         --contrast ${ablation} \
-#         --label-type soft \
-#         -initf 32 -me 200 -bs 2 -opt adam -lr 1e-3 -cve 5 -pat 20 \
-#         -epb --enable_DS \
-#         --loss AdapW
+    # run the model
+    CUDA_VISIBLE_DEVICES=${cuda_device} python monai/main.py \
+        -r /home/GRAMES.POLYMTL.CA/u114716/contrast-agnostic/datalists/spine-generic-contrast-ablations/seed15 \
+        -m nnunet -crop 64x192x320 \
+        --contrast ${ablation} \
+        --label-type soft \
+        -initf 32 -me 200 -bs 2 -opt adam -lr 1e-3 -cve 5 -pat 20 \
+        -epb --enable_DS \
+        --loss AdapW
     
-#     echo "-------------------------------------------------"
-#     echo "Finished training!"
-#     echo "-------------------------------------------------"
-# done
+    echo "-------------------------------------------------"
+    echo "Finished training!"
+    echo "-------------------------------------------------"
+done
 
+# Run inference on the remaining contrasts using hard segmentation labels
+# unseen_contrasts=("dwi" "mtoff" "mton" "t2star")
+unseen_contrasts=("mtoff" "mton")
+
+for contrast in "${unseen_contrasts[@]}"; do
+
+    echo ""
+    echo "-------------------------------------------------"
+    echo "Running inference on contrast: ${contrast}" using hard seg masks
+    echo "-------------------------------------------------"
+
+    # run the model
+    CUDA_VISIBLE_DEVICES=${cuda_device} python monai/run_inference.py \
+        --path-json ~/contrast-agnostic/datalists/spine-generic/seed15/dataset_${contrast}_hard_seed15.json \
+        --chkp-path ~/contrast-agnostic/r1_revision_exps/saved_models/nnunet_t2w_t1w_dwi_t2star_soft_nf=32_opt=adam_lr=0.001_AdapW_bs=2_64x192x320_20240710-2256 \
+        --path-out ~/contrast-agnostic/r1_revision_exps/results/nnunet_t2w_t1w_dwi_t2star_soft_nf=32_opt=adam_lr=0.001_AdapW_bs=2_64x192x320_20240710-2256/preds_${contrast} \
+        --model nnunet \
+        -crop 64x192x-1     
+    
+    echo "-------------------------------------------------"
+    echo "Finished inference!"
+    echo "-------------------------------------------------"
+done
 
 # =================================================================================
 # =================================================================================
@@ -37,27 +61,27 @@
 # different models (on hard labels) for each of the 6 contrasts.
 
 
-contrasts=("t1w" "t2w" "t2star" "dwi" "mtoff" "mton")
-cuda_device=2
+# contrasts=("t1w" "t2w" "t2star" "dwi" "mtoff" "mton")
+# cuda_device=2
 
-for contrast in "${contrasts[@]}"; do
+# for contrast in "${contrasts[@]}"; do
 
-    echo ""
-    echo "-------------------------------------------------"
-    echo "Running model on contrast(s): ${contrast}"
-    echo "-------------------------------------------------"
+#     echo ""
+#     echo "-------------------------------------------------"
+#     echo "Running model on contrast(s): ${contrast}"
+#     echo "-------------------------------------------------"
 
-    # run the model
-    CUDA_VISIBLE_DEVICES=${cuda_device} python monai/main.py \
-        -r /home/GRAMES.POLYMTL.CA/u114716/contrast-agnostic/datalists/spine-generic/seed15 \
-        -m nnunet -crop 64x192x320 \
-        --contrast ${contrast} \
-        --label-type hard \
-        -initf 32 -me 200 -bs 2 -opt adam -lr 1e-3 -cve 10 -pat 20 \
-        -epb --enable_DS \
-        --loss AdapW
+#     # run the model
+#     CUDA_VISIBLE_DEVICES=${cuda_device} python monai/main.py \
+#         -r /home/GRAMES.POLYMTL.CA/u114716/contrast-agnostic/datalists/spine-generic/seed15 \
+#         -m nnunet -crop 64x192x320 \
+#         --contrast ${contrast} \
+#         --label-type hard \
+#         -initf 32 -me 200 -bs 2 -opt adam -lr 1e-3 -cve 10 -pat 20 \
+#         -epb --enable_DS \
+#         --loss AdapW
     
-    echo "-------------------------------------------------"
-    echo "Finished training!"
-    echo "-------------------------------------------------"
-done
+#     echo "-------------------------------------------------"
+#     echo "Finished training!"
+#     echo "-------------------------------------------------"
+# done
