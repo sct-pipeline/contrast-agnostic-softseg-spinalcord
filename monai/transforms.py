12a3f58f75eba7d93e3f55cf6ab1518e219d6c49
Naga Karthik <emvnagakarthik@gmail.com>
2024-08-06 11:15:33 -0400

    increase probability of few existing transforms

--- monai/transforms.py
+++ monai/transforms.py
@@ -18,19 +18,19 @@ def train_transforms(crop_size, lbl_key="label", pad_mode="edge", device="cuda")
         transforms.EnsureTyped(keys=["image", lbl_key], device=device, track_meta=False),
         # data-augmentation
         transforms.RandAffined(keys=["image", lbl_key], mode=(2, 1), prob=0.9,
-                    rotate_range=(-20. / 360 * 2. * np.pi, 20. / 360 * 2. * np.pi),    # monai expects in radians 
-                    scale_range=(-0.2, 0.2),   
+                    rotate_range=(-20. / 360 * 2. * np.pi, 20. / 360 * 2. * np.pi),    # monai expects in radians
+                    scale_range=(-0.2, 0.2),
                     translate_range=(-0.1, 0.1)),
         transforms.Rand3DElasticd(keys=["image", lbl_key], prob=0.5,
-                       sigma_range=(3.5, 5.5), 
-                       magnitude_range=(25., 35.)),
-        transforms.RandSimulateLowResolutiond(keys=["image"], zoom_range=(0.5, 1.0), prob=0.25),
+                       sigma_range=(3.5, 5.5), magnitude_range=(25., 35.),
+                       mode=(2, 1), padding_mode="border",),
+        transforms.RandSimulateLowResolutiond(keys=["image"], zoom_range=(0.5, 1.0), prob=0.5),
         transforms.RandAdjustContrastd(keys=["image"], gamma=(0.5, 3.), prob=0.5),    # this is monai's RandomGamma
         transforms.RandBiasFieldd(keys=["image"], coeff_range=(0.0, 0.5), degree=3, prob=0.3),
         transforms.RandGaussianNoised(keys=["image"], mean=0.0, std=0.1, prob=0.1),
-        transforms.RandGaussianSmoothd(keys=["image"], sigma_x=(0., 2.), sigma_y=(0., 2.), sigma_z=(0., 2.0), prob=0.3),
-        transforms.RandScaleIntensityd(keys=["image"], factors=(-0.25, 1), prob=0.15),  # this is nnUNet's BrightnessMultiplicativeTransform
-        transforms.RandFlipd(keys=["image", lbl_key], prob=0.3,),
+        transforms.RandGaussianSmoothd(keys=["image"], sigma_x=(0., 2.), sigma_y=(0., 2.), sigma_z=(0., 2.0), prob=0.5),
+        transforms.RandScaleIntensityd(keys=["image"], factors=(-0.25, 1), prob=0.35),  # this is nnUNet's BrightnessMultiplicativeTransform
+        transforms.RandFlipd(keys=["image", lbl_key], prob=0.5,),
         transforms.NormalizeIntensityd(keys=["image"], nonzero=False, channel_wise=False),
     ]
 
@@ -53,7 +53,6 @@ def val_transforms(crop_size, lbl_key="label", pad_mode="edge"):
             transforms.LoadImaged(keys=["image", lbl_key], image_only=False),
             transforms.EnsureChannelFirstd(keys=["image", lbl_key]),
             transforms.Orientationd(keys=["image", lbl_key], axcodes="RPI"),
-            # CropForegroundd(keys=["image", lbl_key], source_key="image"),
             transforms.Spacingd(keys=["image", lbl_key], pixdim=(1.0, 1.0, 1.0), mode=(2, 1)), # mode=("bilinear", "bilinear"),),
             transforms.ResizeWithPadOrCropd(keys=["image", lbl_key], spatial_size=crop_size, 
                                             mode="constant" if pad_mode == "zero" else pad_mode),
