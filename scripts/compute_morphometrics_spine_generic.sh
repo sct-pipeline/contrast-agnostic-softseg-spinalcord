36f9fd9462948222b1b1cc6bf378ce11d5379254
Naga Karthik <emvnagakarthik@gmail.com>
2025-03-07 21:37:53 -0500

    add standalone script to compute morphometrics given a model

---
+++ scripts/compute_morphometrics_spine_generic.sh
@@ -0,0 +1,125 @@
+#!/bin/bash
+# Generic post-training script for evaluating morphometric drift of a lifelong learning contrast-agnostic spinal cord segmentation model. 
+# It assumes that git-annex and Spinal Cord Toolbox (https://spinalcordtoolbox.com/) are installed. 
+# 
+# This standalone script that performs the following tasks:
+# 1. Clones the spine-generic `data-multi-subject` (https://github.com/spine-generic/data-multi-subject) dataset
+# 2. Downloads the data only for the subjects listed in `test_split_for_csa_drift_monitoring.yaml`
+# 3. Runs a batch analysis (sct_run_batch) to compute the spinal cord cross-sectional area (CSA) on the test 
+#   subjects downloaded in step 2 using a given version of the contrast-agnostic model.
+# 4. Stores the output CSV files in the current directory, to be uploaded as part of the next release of the model
+# 
+# Usage:
+#   bash compute_morphometrics_spine_generic.sh
+
+
+# ==============================
+# DEFINE GLOBAL VARIABLES
+# ==============================
+# Path to the repository; all scripts will be relative to this path
+PATH_REPO="/home/GRAMES.POLYMTL.CA/${USER}/contrast-agnostic/contrast-agnostic-softseg-spinalcord"
+
+# Path to the output folder; the data, model, results, etc. will be stored in this folder
+PATH_OUTPUT="/home/GRAMES.POLYMTL.CA/${USER}/contrast-agnostic/test-post-training-script"
+
+# Path to the folder where the model exists, will be copied to the output folder PATH_OUTPUT
+# for testing purposes, replace the PATH_MODEL with the path to the model downloaded from the latest release
+PATH_MODEL="/home/GRAMES.POLYMTL.CA/${USER}/contrast-agnostic/sct_deployed_models/model_nnunet-AllRandInit3D"
+
+# NOTE: To be compatible previous releases of the models, and to be able to automatically generate the
+# morphometric plots after a new model is released, the model folder after copying will have the following
+# syntax: ${PATH_OUTPUT}/model_${VERSION_TO_BE_RELEASED}
+# Example: If the latest release in the contrast-agnostic repo points to the tag v3.1,
+# then the next version to be released is v3.2. 
+VERSION_TO_BE_RELEASED=3.2
+
+# Number of parallel processes to run (choose a smaller number as inference is run only on 1 gpu)
+NUM_WORKERS=4
+
+# ID of the GPU to run inference on {0,1,2,3}
+CUDA_DEVICE_ID=3
+
+# Create folders 
+mkdir -p "${PATH_OUTPUT}"
+mkdir -p "${PATH_OUTPUT}/model_v${VERSION_TO_BE_RELEASED}"
+
+# Copy the model to the output folder
+cp -r ${PATH_MODEL}/* ${PATH_OUTPUT}/model_v${VERSION_TO_BE_RELEASED}
+
+# Exit if user presses CTRL+C (Linux) or CMD+C (OSX)
+trap "echo Caught Keyboard Interrupt within script. Exiting now.; exit" INT
+
+# ==============================
+# DOWNLOAD DATA
+# ==============================
+
+echo "=============================="
+echo "Downloading test data ..."
+echo "=============================="
+
+# Clone the dataset and initialize a git annex repository
+git clone https://github.com/spine-generic/data-multi-subject ${PATH_OUTPUT}/data-multi-subject
+cd ${PATH_OUTPUT}/data-multi-subject
+
+# Initialize an empty git-annex repository 
+git annex init
+
+# Get the (frozen) test split of the spine-generic dataset to compute the morphometrics on
+# And download them using git-annex
+python3 -c 'import yaml, sys; 
+test_subjects = yaml.safe_load(open(sys.argv[1]))["test"]; 
+for subject in test_subjects: print(subject)' ${PATH_REPO}/scripts/spine_generic_test_split_for_csa_drift_monitoring.yaml | 
+while IFS= read -r subject; do
+    echo "Downloading: $subject"
+    # download images
+    git annex get "${subject}"
+    # change directory to derivatives
+    cd ${PATH_OUTPUT}/data-multi-subject/derivatives
+    # download all kinds of labels
+    git annex get $(find . -name "${subject}")
+    # change back to root directory
+    cd ..
+done
+
+# Return to the root directory of the repo
+cd ${PATH_REPO}
+
+
+# ==============================
+# RUN BATCH ANALYSIS
+# NOTE: this section piggybacks on the sct_run_batch argument provided by SCT
+# Instead of providing a config file for batch processing script, we will provide the input arguments below
+# ==============================
+
+# Get the list of test subjects in a list
+readarray -t TEST_SUBJECTS < <(python3 -c 'import yaml, sys; 
+test_subjects = yaml.safe_load(open(sys.argv[1]))["test"]; 
+for subject in test_subjects: print(subject)' "${PATH_REPO}/scripts/test_split_for_csa_drift_monitoring.yaml")
+# echo "${TEST_SUBJECTS[@]}"
+
+echo "=============================="
+echo "Running batch analysis ..."
+echo "=============================="
+
+# Run batch processing
+todays_date=$(date +"%Y%m%d")
+path_out_run_batch=${PATH_OUTPUT}/${todays_date}__results_csa__model_v${VERSION_TO_BE_RELEASED}
+
+sct_run_batch -path-data ${PATH_OUTPUT}/data-multi-subject \
+    -path-output ${path_out_run_batch} \
+    -jobs ${NUM_WORKERS} \
+    -script ${PATH_REPO}/scripts/compute_csa.sh \
+    -script-args "${CUDA_DEVICE_ID} ${PATH_REPO}/nnUnet/run_inference_single_subject.py ${PATH_OUTPUT}/model_v${VERSION_TO_BE_RELEASED}" \
+    -include-list ${TEST_SUBJECTS[@]}
+
+
+echo "=============================="
+echo "Moving results to ${PATH_REPO}/csa_qc_evaluation_spine_generic ..."
+echo "=============================="
+
+cp -r ${path_out_run_batch}/results/csa_c2c3.csv ${PATH_REPO}/csa_qc_evaluation_spine_generic
+
+echo "=============================="
+echo "Morphometrics computation done!"
+echo "Upload the CSV file along with the release to compare CSA drift with respect to previous models."
+echo "=============================="
