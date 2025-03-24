b9feffb1a4b71562ea149eac2fb2397f96bb77af
Naga Karthik <emvnagakarthik@gmail.com>
2025-03-24 16:12:06 -0400

    change -install task to spinalcord

--- scripts/compute_morphometrics_spine_generic.sh
+++ scripts/compute_morphometrics_spine_generic.sh
@@ -48,7 +48,7 @@ echo "=============================="
 echo "Downloading model from URL ${MODEL_URL} ..."
 echo "=============================="
 
-sct_deepseg -install seg_sc_contrast_agnostic -custom-url ${MODEL_URL}
+sct_deepseg -install spinalcord -custom-url ${MODEL_URL}
 
 echo "Model download complete."
 
